b62b8e8c18badd25c201970e70bd4eff12e4b697
tim-ahn <tim.ahn3@gmail.com>
2020-10-13 02:30:43 +0000

    added state hook for availableCash and added cash balance to navbar

--- components/buyingPage.js
+++ components/buyingPage.js
@@ -2,9 +2,17 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-
+import stocks from './stocks.json'
 
 export default function BuyingPage() {
+  // 🤔
+  // const ownedStocks = []
+  // for (let i = 0; i < stocks.length; i++) {
+  //   if (stocks[i].owned > 0) {
+  //     ownedStocks.push(stocks[i])
+  //   }
+  // }
+
   return (
     <>
       <Container>
@@ -12,7 +20,19 @@ export default function BuyingPage() {
         <Box my={4}>
           <h1>Buying</h1>
         </Box>
+
+
+
       </Container>
+
+      {/* 🤔
+      <ul>
+        {ownedStocks.map(stock => {
+          <li key={stock.id}>
+            {stock.name}
+          </li>
+        })}
+      </ul> */}
     </>
   );
 }
