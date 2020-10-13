f4ad10594c41c0c6b998b8577572a650d2a69998
tim-ahn <tim.ahn3@gmail.com>
2020-10-13 03:44:21 +0000

    rendered list of stocks on buyingPage component

--- components/buyingPage.js
+++ components/buyingPage.js
@@ -2,17 +2,34 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-
+import stocks from './stocks.json'
+import { List, ListItem, Button, ListItemSecondaryAction, ListItemText } from '@material-ui/core';
 
 export default function BuyingPage() {
+let stockList = []
+  for(let i=0; i<stocks.length; i++){
+  stockList.push(
+    <ListItem>
+      <ListItemText
+        primary={stocks[i].name}
+        secondary={stocks[i].cost}
+      />
+      <ListItemSecondaryAction>
+        <Button variant="contained" color="primary">Buy</Button>
+      </ListItemSecondaryAction>
+    </ListItem>
+ )}
+
   return (
     <>
       <Container>
-
         <Box my={4}>
           <h1>Buying</h1>
         </Box>
       </Container>
+        <List >
+          {stockList}
+        </List>
     </>
   );
 }
