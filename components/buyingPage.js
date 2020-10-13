758325dfb04c8d52e9f93b61e889db771802ec84
david-diep <ddkotan@gmail.com>
2020-10-12 18:51:04 -0700

    no more pages, using views

---
+++ components/buyingPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
+
+export default function BuyingPage() {
+  return (
+    <>
+      <Container>
+
+        <Box my={4}>
+          <h1>Buying</h1>
+        </Box>
+      </Container>
+    </>
+  );
+}
