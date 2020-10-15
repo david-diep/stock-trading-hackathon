d20daf09071acd7bc714ae21f0bec2a24ff66e93
tim-ahn <tim.ahn3@gmail.com>
2020-10-15 07:06:43 +0000

    added tooltips to stockSellListItem component

--- components/stockPurchaseListItem.js
+++ components/stockPurchaseListItem.js
@@ -1,7 +1,7 @@
 import { makeStyles } from '@material-ui/core/styles';
 import React, { useState } from 'react';
 import Tooltip from '@material-ui/core/Tooltip';
-import {  ListItem, Button, ListItemSecondaryAction, ListItemText, TextField } from '@material-ui/core';
+import { ListItem, Button, ListItemSecondaryAction, ListItemText, TextField } from '@material-ui/core';
 import { useDispatch } from 'react-redux'
 import { stockOwnedChange } from '../redux/actions/stocksActions'
 
@@ -17,33 +17,33 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-export default function StockPurchaseListItem(props){
+export default function StockPurchaseListItem(props) {
   const stock = props.stock;
   const classes = useStyles();
   const dispatch = useDispatch();
   const [quantity, setQuantity] = useState(0)
 
-  const max =  Math.floor(props.money/stock.price)
-  function handlePurchase(){
-    if(quantity*stock.price <=props.money){
+  const max = Math.floor(props.money / stock.price)
+  function handlePurchase() {
+    if (quantity * stock.price <= props.money) {
       dispatch(stockOwnedChange(stock.id, quantity + stock.owned));
     }
 
   }
 
-  return(
+  return (
     <>
       <ListItem
         key={stock.id}>
         <Tooltip placement="bottom-start" title="Stock name and price" aria-label="Stock name and price">
-        <ListItemText
-          className={classes.stockText}
-          primary={stock.name}
-          secondary={'$' + stock.price} /></Tooltip>
+          <ListItemText
+            className={classes.stockText}
+            primary={stock.name}
+            secondary={'$' + stock.price} /></Tooltip>
         <Tooltip placement="left" title="Enter purchase amount" aria-label="purchase quantity">
           <TextField required
             onChange={event => { setQuantity(parseInt(event.target.value)) }}
-            style={{width:'20%'}}
+            style={{ width: '20%' }}
             InputProps={{
               inputProps: {
                 max: max, min: 1
@@ -54,13 +54,13 @@ export default function StockPurchaseListItem(props){
             variant="outlined"
             type="number"
             className={classes.quantityField} />
-          </Tooltip>
+        </Tooltip>
         <Tooltip placement="top-end" title="Buy button" aria-label="Buy button">
-        <Button
-          className={classes.buyButton}
-          variant="contained"
-          color="primary"
-        onClick={() => handlePurchase()}
+          <Button
+            className={classes.buyButton}
+            variant="contained"
+            color="primary"
+            onClick={() => handlePurchase()}
           >Buy</Button></Tooltip>
       </ListItem>
     </>
--- components/stockSellListItem.js
+++ components/stockSellListItem.js
@@ -4,6 +4,7 @@ import Chip from '@material-ui/core/Chip';
 import { ListItem, Button, ListItemSecondaryAction, ListItemText, TextField } from '@material-ui/core';
 import { useDispatch } from 'react-redux'
 import { stockOwnedChange } from '../redux/actions/stocksActions'
+import Tooltip from '@material-ui/core/Tooltip';
 
 const useStyles = makeStyles((theme) => ({
   sellButton: {
@@ -31,37 +32,41 @@ export default function StockPurchaseListItem(props) {
 
 
   function handleSell() {
-      dispatch(stockOwnedChange(stock.id, Math.max(0, stock.owned-quantity)));
+    dispatch(stockOwnedChange(stock.id, Math.max(0, stock.owned - quantity)));
   }
 
   return (
     <>
       <ListItem
         key={stock.id}>
-        <ListItemText
-          className={classes.stockText}
-          primary={stock.name}
-          secondary={'$' + stock.price} />
-        <Chip size="small" label={stock.owned} className={classes.chipColor} />
-        <TextField required
-          onChange={event => { setQuantity(parseInt(event.target.value)) }}
-          style={{ width: '20%' }}
-          InputProps={{
-            inputProps: {
-              max: stock.owned, min: 1
-            }
-          }}
-          id={stock.id}
-          label="Quantity"
-          variant="outlined"
-          type="number"
-          className={classes.quantityField} />
-        <Button
-          className={classes.buyButton}
-          variant="contained"
-          color="primary"
-          onClick={() => handleSell()}
-        >Sell</Button>
+        <Tooltip placement="bottom-start" title="Stock name and price" aria-label="Stock name and price">
+          <ListItemText
+            className={classes.stockText}
+            primary={stock.name}
+            secondary={'$' + stock.price} /></Tooltip>
+        <Tooltip placement="left" title="Quantity owned" aria-label="Quantity owned">
+          <Chip size="small" label={stock.owned} className={classes.chipColor} /></Tooltip>
+        <Tooltip placement="bottom" title="Enter amount to sell" aria-label="Enter amount to sell">
+          <TextField required
+            onChange={event => { setQuantity(parseInt(event.target.value)) }}
+            style={{ width: '20%' }}
+            InputProps={{
+              inputProps: {
+                max: stock.owned, min: 1
+              }
+            }}
+            id={stock.id}
+            label="Quantity"
+            variant="outlined"
+            type="number"
+            className={classes.quantityField} /></Tooltip>
+        <Tooltip placement="bottom" title="Sell button" aria-label="Sell button">
+          <Button
+            className={classes.buyButton}
+            variant="contained"
+            color="primary"
+            onClick={() => handleSell()}
+          >Sell</Button></Tooltip>
       </ListItem>
     </>
   )
